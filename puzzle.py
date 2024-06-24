6108614aa6012c90073786ee10f2b46d6d7ad48c
C10uld <nnn.notnow@gmail.com>
2024-06-24 14:40:28 +0900

    find blank

--- puzzle.py
+++ puzzle.py
@@ -1,3 +1,4 @@
+# puzzleList Create
 iNum, jNum = 4, 4
 puzzleList = []
 # [[1, 5, 9, 13], [2, 6, 10, 14],...]
@@ -11,9 +12,28 @@ for i in range(iNum):
     puzzleList.append(tempList)
 puzzleList[-1][-1] = 0
 
-textTemp = ""
-for j in range(jNum):
-    for i in range(iNum):
-        textTemp += str(puzzleList[i][j]) + "\t"
-    textTemp += "\n"
-print(textTemp)
+# puzzleList Show
+def puzzleShow():
+    textTemp = ""
+    for j in range(jNum):
+        for i in range(iNum):
+            textTemp += str(puzzleList[i][j]) + "\t"
+        textTemp += "\n"
+    print(textTemp)
+
+# find blank
+for i in range(iNum):
+    for j in range(jNum):
+        if puzzleList[i][j] == 0:
+            blank = (i, j)
+print(blank)
+dirDict = {
+    "left": (-1, 0), "right": (1, 0), "up": (0, -1), "down": (0, 1) # 이동 방향
+}
+i, j = blank # 빈칸의 좌표
+ii, jj = dirDict["left"] # 퍼즐의 이동 방향
+iNew, jNew = i+ii, j+jj # 이동 후 좌표
+if iNew >= 0 and iNew < iNum and jNew >= 0 and jNew < jNum:
+    puzzleList[i][j], puzzleList[iNew][jNew] = \
+        puzzleList[iNew][jNew], puzzleList[i][j]
+puzzleShow()
